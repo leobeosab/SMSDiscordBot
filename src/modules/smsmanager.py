6fbfc67525db32756911422a9264a442c79c7c7a
Ryan Wise <ryan@ryanwise.me>
2019-06-26 01:14:29 -0400

    add batch function

--- src/modules/smsmanager.py
+++ src/modules/smsmanager.py
@@ -3,6 +3,7 @@ from twilio.rest import Client
 # Handy manager for managing Twilio Messages
 class Manager:
 
+  #possible read twilio info from API at some point
   def __init__(self, twilioSid, twilioToken, fromNumber):
     self.client = Client(twilioSid, twilioToken)
     self.fromNumber = fromNumber
@@ -10,3 +11,7 @@ class Manager:
   def send_text_message(self, toPhone, message):
     message = self.client.messages.create(to=toPhone, from_=self.fromNumber, body=message)
     return(message.sid)
+
+  def send_batch_message(self, recipients, message):
+    for recipient in recipients:
+      self.client.messages.create(to=recipient, from_=self.fromNumber, body=message)

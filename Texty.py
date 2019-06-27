0bf40db55866ea3a0b65feb76d6ddf00af893dc8
Ryan Wise <ryan@ryanwise.me>
2019-06-27 00:05:53 -0400

    Code style fix, remove client (not needed)

--- Texty.py
+++ Texty.py
@@ -1,42 +1,32 @@
 import discord, os
 from discord.ext import commands
 from discord.utils import get
-from discord import member
+from discord import Client
 
+# Creates bot object, with command prefix to use like : $testCommand arg0 arg1 arg2
+BOT = commands.Bot(command_prefix ='$')
 
+@BOT.event
+async def on_ready():
+    print("Logged in User: {0.user} | {0.user.id}".format(BOT))
 
-bot = commands.Bot(command_prefix ='.')
-
-class MyClient(discord.Client):
-
-
+@BOT.event
+async def on_message(message):
+    #prevents bot from responding to self
+    if message.author.id == BOT.user.id:
+        return
     
-    async def on_ready(self):
-
-        print('ready')
+    if message.content.lower().startswith("hello <@{0}>".format(BOT.user.id)):
+        await message.channel.send('Hi {0.author.mention}'.format(message))
     
-    
-    async def on_message(self, message):
+    if message.content.lower().startswith('list roles'):
+        roles = ['']
         
+        for member in message.server.members:
+            for role in member.roles:
+                    if not role in roles:
+                        roles += role
         
+        await message.channel.send('%d' %roles)
 
-        #prevents bot from responding to self
-        if message.author.id == self.user.id:
-            return
-        
-        if message.content.lower().startswith("hello <@592914379964678155>"):
-            await message.channel.send('Hi {0.author.mention}'.format(message))
-        
-        if message.content.lower().startswith('list roles'):
-            roles = ['']
-            
-            for member in message.server.members:
-                for role in member.roles:
-                        if not role in roles:
-                            roles += role
-            
-            await message.channel.send('%d' %roles)
-
-
-client = MyClient()
-client.run(os.environ['TEXTEEBOTTOKEN'])
+BOT.run(os.environ['TEXTEEBOTTOKEN'])
